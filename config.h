13e84bb4687139017eedc0eb467b1efa78a5fdde
Arminder Singh (amarioguy) <arminders208@outlook.com>
2025-06-22 18:51:03 -0400

    add an ifdef for vGIC module enablement for debug reasons
    Signed-off-by: Arminder Singh (amarioguy) <arminders208@outlook.com>

--- config.h
+++ config.h
@@ -27,6 +27,11 @@
 // #define ENABLE_SMMU
 
 //
+// Enable the vGIC module.
+//
+// #define ENABLE_VGIC_MODULE
+
+//
 // Use PSCI to turn on the CPUs in earnest rather than just setting up the spintables that m1n1 uses.
 //
 // #define PSCI_POWER_ON_CPUS_ENABLE
